1eeaf9a6969ddb396c35da135976035e83495049
chaojunyang <chaojunyang@yunify.com>
2022-09-19 08:16:20 +0800

    chore: 引入antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.less';
+import './index.less';
+
+moment.locale('zh-cn');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
